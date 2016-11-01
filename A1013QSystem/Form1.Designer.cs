ebc72f28de6b6c742e460e0d15075dc61593f4b2
Jasen <295062088@qq.com>
2016-11-01 15:20:52 +0800

    yjx

--- A1013QSystem/Form1.Designer.cs
+++ A1013QSystem/Form1.Designer.cs
@@ -101,7 +101,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(142, 181);
+            this.button2.Location = new System.Drawing.Point(142, 172);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 31);
             this.button2.TabIndex = 11;
@@ -189,7 +189,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(142, 175);
+            this.button1.Location = new System.Drawing.Point(142, 170);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 5;
