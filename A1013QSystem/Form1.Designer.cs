de870536963568246536529c6620062e7722f044
Jasen <295062088@qq.com>
2016-11-01 15:13:42 +0800

    yjx

--- A1013QSystem/Form1.Designer.cs
+++ A1013QSystem/Form1.Designer.cs
@@ -262,7 +262,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(52, 50);
+            this.label7.Location = new System.Drawing.Point(40, 50);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(116, 20);
             this.label7.TabIndex = 12;
